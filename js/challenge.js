724fb5748b444811f67a41ee9ac415b682afc739
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-22 18:07:07 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -17,7 +17,7 @@ const decrementor = document.getElementById("minus");
 const incrementor = document.getElementById("plus");
 const liker = document.getElementById("heart");
 const pauseToggler = document.getElementById("pause");
-const commentSubmitter = document.querySelector("button#sumbit"); //not sure why this doesn't work, but it doesn't
+//const commentSubmitter = document.querySelector("button#sumbit"); //not sure why this doesn't work, but it doesn't
 const commentSubmitter = document.querySelector("form#comment-form button");
 const eventListeningNodes = [decrementor, incrementor, liker, commentSubmitter]; //to disable when paused
 
